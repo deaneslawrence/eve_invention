e72823c985fa7ca57e1bc20c285167ef411c8866
Deane Lawrence <deaneslawrence@gmail.com>
2014-04-05 00:52:16 -0400

    Init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EveInvention::Application.config.secret_key_base = 'f5124279b42e3b000addd074ac9d12a338248a012c1fb85a60a78efdd211724556867e30e8500685ab3cb5326c6d5b45f10d8729ab6ee2950b0d392dae7e0c2e'
