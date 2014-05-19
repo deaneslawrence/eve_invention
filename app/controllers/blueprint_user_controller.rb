d425f974ff57088d613916cd2619607e59b0bd97
Deane Lawrence <deaneslawrence@gmail.com>
2014-05-19 01:56:55 -0400

    Add SSL in production

--- app/controllers/blueprint_user_controller.rb
+++
@@ -1,2 +0,0 @@
-class BlueprintUserController < ApplicationController
-end
--- db/migrate/20140427175930_create_blueprint_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateBlueprintUsers < ActiveRecord::Migration
-  def change
-    create_table :blueprint_users do |t|
-
-      t.timestamps
-    end
-  end
-end
